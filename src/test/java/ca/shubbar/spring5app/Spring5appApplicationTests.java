bf423cb59f01a9d6ec1433219e9f5decc0678542
Mustafa <codingbox@outlook.com>
2021-08-11 20:56:47 -0400

    init

---
+++ src/test/java/ca/shubbar/spring5app/Spring5appApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.shubbar.spring5app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
