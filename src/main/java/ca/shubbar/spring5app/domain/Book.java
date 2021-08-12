1f3a4db7512a1e83846bc98ee85727c1ea12c798
Mustafa <codingbox@outlook.com>
2021-08-11 21:24:27 -0400

    2 POJOs for the JPA, Authors & Books

---
+++ src/main/java/ca/shubbar/spring5app/domain/Book.java
@@ -0,0 +1,46 @@
+package ca.shubbar.spring5app.domain;
+
+import java.util.Set;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-11
+ */
+public class Book {
+    private String title;
+    private String isbn;
+    private Set<Author> authors;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+}
